b37ff8f468c799e83483dba7640e047d9ee36cb0
Max <arcas.llc@yandex.ru>
2017-10-17 17:38:14 +0300

    refactoring

--- src/main/java/main/controller/DesignerController.java
+++ src/main/java/main/controller/DesignerController.java
@@ -30,52 +30,52 @@ public class DesignerController {
 	@RequestMapping(value = {"/designer"}, method = RequestMethod.GET)
 	public ModelAndView designer() {
 		ModelAndView model = new ModelAndView("/designerView/DesignerDashBoard");
-		try{
-			model.addObject("orders", orderService.designerOrders());
-		}
-		catch (Exception e){
-			logger.error("Controller '/designer', orderService.designerOrders() error ");
-		}
+			try{
+				model.addObject("orders", orderService.designerOrders());
+			}
+			catch ( Exception e ){
+				logger.error("Controller '/designer', orderService.designerOrders() error ");
+			}
 		return model;
 	}
 
 	@RequestMapping(value = {"/designer/order"}, method = RequestMethod.GET)
 	public ModelAndView order(HttpServletRequest request) {
 		ModelAndView model = new ModelAndView("/designerView/DesignerOrder");
-		try{
-			String id = request.getParameter("orderId");
-			model.addObject("order", orderService.get(Long.parseLong(id)));
-		} catch (Exception e){
-			model = new ModelAndView("/designerView/DesignerDashBoard");
-			logger.error("Controller '/designer/order', orderId={}", request.getParameter("orderId"));
-		}
+			try{
+				String id = request.getParameter("orderId");
+				model.addObject("order", orderService.get(Long.parseLong(id)));
+			} catch (Exception e){
+				model = new ModelAndView("/designerView/DesignerDashBoard");
+				logger.error("Controller '/designer/order', orderId={}", request.getParameter("orderId"));
+			}
 		return model;
 	}
 
 	@RequestMapping(value = {"/designer/order/item"}, method = RequestMethod.GET)
 	public ModelAndView item(HttpServletRequest request) {
 		ModelAndView model = new ModelAndView("/designerView/DesignerItem");
-		try {
-			model.addObject("item", itemService.get(Long.parseLong(request.getParameter("itemId"))));
-		}
-		catch (Exception e) {
-			logger.error("Controller '/designer/order/item', itemId={}", request.getParameter("itemId"));
-			 model = new ModelAndView("/designerView/DesignerDashBoard");
-		}
+			try {
+				model.addObject("item", itemService.get(Long.parseLong(request.getParameter("itemId"))));
+			}
+			catch (Exception e) {
+				logger.error("Controller '/designer/order/item', itemId={}", request.getParameter("itemId"));
+				 model = new ModelAndView("/designerView/DesignerDashBoard");
+			}
 		return model;
 	}
 
 	@RequestMapping(value = {"/designer/search"}, method = RequestMethod.POST)
 	public ModelAndView search(HttpServletRequest request) {
 		ModelAndView model = new ModelAndView("/designerView/DesignerDashBoard");
-		try{
-			String search = request.getParameter("search");
-			model.addObject("orders", orderService.designFindNumber(search));
-		}
-		catch (Exception e){
-			logger.error("Controller '/designer/search', search={}", request.getParameter("search"));
-			model = new ModelAndView("/designerView/DesignerDashBoard");
-		}
+			try{
+				String search = request.getParameter("search");
+				model.addObject("orders", orderService.designFindNumber(search));
+			}
+			catch (Exception e){
+				logger.error("Controller '/designer/search', search={}", request.getParameter("search"));
+				model = new ModelAndView("/designerView/DesignerDashBoard");
+			}
 		return model;
 	}
 
@@ -83,18 +83,18 @@ public class DesignerController {
 	public ModelAndView save(@PathVariable Long id, HttpServletRequest request) {
 		ModelAndView model = new ModelAndView("/designerView/DesignerItem");
 		String status = request.getParameter("status");
-		try{
-			Item item = itemService.get(id);
-			if (status != null) {
-				item.setStatus(status);
-				itemService.save(item);
-			}
+			try{
+				Item item = itemService.get(id);
+				if (status != null) {
+					item.setStatus(status);
+					itemService.save(item);
+				}
 			model.addObject("item", item);
-		}
-		catch (Exception e) {
-			logger.error("Controller '/designer/order/item/save/', id={}", id);
-			model = new ModelAndView("/designerView/DesignerDashBoard");
-		}
+			}
+			catch (Exception e) {
+				logger.error("Controller '/designer/order/item/save/', id={}", id);
+				model = new ModelAndView("/designerView/DesignerDashBoard");
+			}
 		return model;
 	}
 
@@ -102,9 +102,9 @@ public class DesignerController {
 	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public String uploadSampleFiles(@RequestParam(value = "file") MultipartFile file) {
+			String name = "drop";
 
-		String name = "drop";
-		if (!file.isEmpty()) {
+			if (!file.isEmpty()) {
 			name = file.getOriginalFilename();
 
 			try {
