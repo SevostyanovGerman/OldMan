69aa109cab2c321eee3fb1fbce3e3ffe567ce39a
Dima Ogorodnikov <kolizer@yandex.ru>
2017-10-18 23:04:53 +0300

    Done the following work: 1) Create CustomerRepository 2) Create CustomerService and CustomerServiceImpl 3) Update model Customer:

---
+++ src/main/java/main/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package main.repository;
+
+/**
+ * Created by Администратор on 17.10.2017.
+ */
+public interface CustomerRepository {
+}
---
+++ src/main/java/main/service/CustomerServiceImpl.java
@@ -0,0 +1,7 @@
+package main.service;
+
+/**
+ * Created by Администратор on 18.10.2017.
+ */
+public class CustomerServiceImpl {
+}
