aa8942f4e588f753fdf06decbdd7f0adb1c0c43b
Max <arcas.llc@yandex.ru>
2017-10-18 13:20:39 +0300

    refactoring

--- src/main/java/main/service/HistoryServiceImpl.java
+++ src/main/java/main/service/HistoryServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Date;
 
-
 @Service
 @Transactional
 public class HistoryServiceImpl implements HistoryService {
