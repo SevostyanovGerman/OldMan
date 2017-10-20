57692ce64cc7daaf785e004ca36bc70096eddd19
RuslanZinovyev <ruslan.zinovyev@hotmail.com>
2017-10-21 02:18:54 +0500

    MasterController with status master check has been added

---
+++ src/main/java/main/controller/MasterController.java
@@ -0,0 +1,62 @@
+package main.controller;
+
+import main.model.Item;
+import main.model.Order;
+import main.service.ItemService;
+import main.service.OrderService;
+import main.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MasterController {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    UserService userService;
+
+    List<Order> masters = new ArrayList<>();
+
+
+    @RequestMapping(value = {"/master"}, method = RequestMethod.GET)
+    public String getMasterDashBoard(Model model) {
+        List<Order> allOrders = orderService.getAll();
+        for (Order order : allOrders) {
+            if (order.getStatus().getId() == 2) {
+                masters.add(order);
+            }
+        }
+          model.addAttribute("masters", masters);
+
+        return "masterView/MasterDashBoard";
+    }
+
+    @RequestMapping(value = {"/master/order/{id}"}, method = RequestMethod.GET)
+    public String getOrderForm(@PathVariable ("id") Long id, Model model) {
+        Order order = orderService.get(id);
+        List<Item> allItems = order.getItems();
+        model.addAttribute("order", order);
+        model.addAttribute("allItems", allItems);
+        return "masterView/MasterOrderForm";
+    }
+
+    @RequestMapping(value = {"/master/item/{id}"}, method = RequestMethod.GET)
+    public String getItemForm(@PathVariable ("id") Long id, Model model) {
+        Item item = itemService.get(id);
+        model.addAttribute(item);
+        return "masterView/MasterItemForm";
+    }
+
+}
