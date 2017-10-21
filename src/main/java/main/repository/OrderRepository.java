cdd789f8c20a84c1144ec840e78079abc46423c6
RuslanZinovyev <ruslan.zinovyev@hotmail.com>
2017-10-21 15:23:28 +0500

    Has been added new search method to MasterController class (perform search operations)

--- src/main/java/main/repository/OrderRepository.java
+++ src/main/java/main/repository/OrderRepository.java
@@ -2,6 +2,9 @@ package main.repository;
 
 import main.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -12,4 +15,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByDeletedAndManagerFirstNameContains(int deleted, String name);
     List<Order> findAllByDeletedAndStatusId(int deleted, Long id);
     List<Order> findAllByDeletedAndStatusIdAndNumberContains(int deleted, Long statusId, String number);
+    @Query("SELECT o FROM Order o WHERE " +
+            "LOWER(o.payment) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
+            "LOWER(o.dateRecieved) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
+            "LOWER(o.dateTransferred) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
+            "LOWER(o.deliveryType) LIKE LOWER(CONCAT('%',:searchTerm, '%'))")
+    List<Order> findBySearchTerm(@Param("searchTerm") String searchTerm);
 }
