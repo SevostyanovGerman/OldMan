d17636532a7b175e60920628f586871e71383162
Max <arcas.llc@yandex.ru>
2017-10-22 17:46:25 +0300

    refactoring

--- src/main/java/main/model/Image.java
+++ src/main/java/main/model/Image.java
@@ -1,7 +1,5 @@
 package main.model;
 
-
-
 import javax.imageio.ImageIO;
 import javax.persistence.*;
 import javax.xml.bind.DatatypeConverter;
@@ -13,74 +11,56 @@ import java.sql.Blob;
 import java.sql.SQLException;
 
 @Entity
-@Table(name="images")
+@Table(name = "images")
 public class Image {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+	@Column(name = "image")
+	private Blob image;
 
-    @Column(name = "url")
-    private String url;
+	public Long getId() {
+		return id;
+	}
 
-    @Column(name = "image")
-    private Blob image;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getImage() throws IOException, SQLException {
+		if (this.image != null) {
+			InputStream in = this.image.getBinaryStream();
+			BufferedImage image = ImageIO.read(in);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(image, "png", baos);
+			String data = DatatypeConverter.printBase64Binary(baos.toByteArray());
+			String imageString = "data:image/png;base64," + data;
+			String html = imageString;
+			return html;
+		}
+		return null;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setImage(Blob image) {
+		this.image = image;
+	}
 
-    public String getUrl() {
-        return url;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Image image1 = (Image) o;
+		if (id != null ? !id.equals(image1.id) : image1.id != null)
+			return false;
+		return image != null ? image.equals(image1.image) : image1.image == null;
+	}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getImage() throws IOException, SQLException {
-        if (this.image != null){
-            InputStream in = this.image.getBinaryStream();
-            BufferedImage image = ImageIO.read(in);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", baos);
-            String data = DatatypeConverter.printBase64Binary(baos.toByteArray());
-            String imageString = "data:image/png;base64," + data;
-            String html = imageString;
-            return html;
-        }
-     return null;
-    }
-
-    public void setImage(Blob image) {
-        this.image = image;
-    }
-
-    @Override
-    public String toString() {
-        return url;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Image image = (Image) o;
-
-        if (id != null ? !id.equals(image.id) : image.id != null) return false;
-        return url != null ? url.equals(image.url) : image.url == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (image != null ? image.hashCode() : 0);
+		return result;
+	}
 }
