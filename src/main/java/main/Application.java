042056deb50f761e5e64e799de3e0ba421053996
Max <arcas.llc@yandex.ru>
2017-10-25 15:35:28 +0300

    refactoring

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,9 +1,7 @@
 package main;
 
-import main.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
-
-	@Autowired
-	UserService userService;
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
