02c4b775b74ebaea493adadaa39fa53dc023e791
Dmitriy Lakhno <dimalahno@rambler.ru>
2017-11-01 12:20:09 +0600

    1) hotfix: add @Repository annotation

--- src/main/java/main/repository/ItemRepository.java
+++ src/main/java/main/repository/ItemRepository.java
@@ -2,7 +2,9 @@ package main.repository;
 
 import main.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository <Item, Long> {
 
 	Item save(Item item);
--- src/main/java/main/repository/OrderRepository.java
+++ src/main/java/main/repository/OrderRepository.java
@@ -5,8 +5,11 @@ import main.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository <Order, Long> {
 
 	Order findByIdAndDeleted(Long id, Boolean deleted);
