9d47595bd73b057f7e472d53780723b108397f14
Max <arcas.llc@yandex.ru>
2017-11-06 23:41:11 +0300

    add: -js mxItemManager
    refactoring ManagerOrderForm

---
+++ src/main/resources/static/js/mxItemManager.js
@@ -0,0 +1,26 @@
+<!--Считаем сумму позиции-->
+
+function amountItem() {
+    var amount = document.getElementById("amountItemField");
+    var price = document.getElementById("priceItemField");
+    var number = document.getElementById("numberItemField");
+    var btn = document.getElementById("saveItemBtn");
+
+    try{
+        if (price.value != 0 & number.value != 0) {
+            amount.value = price.value * number.value ;
+            if (isNaN(amount.value)) {
+                btn.disabled = true;
+                amount.value = "";
+            } else {
+                btn.disabled = false;
+            }
+        } else {
+            amount.value = "";
+            btn.disabled = true;
+        }
+
+    } catch (e){
+
+    }
+}
