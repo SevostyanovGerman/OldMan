16a60de878657849e97fc4817f644f335d405074
Ruslan Zinovyev <ruslan.zinovyev@hotmail.com>
2018-01-04 18:14:47 +0500

    Has been solved critical issues with typehead and server notification errors

--- src/main/resources/static/js/masterOrderModule.js
+++ src/main/resources/static/js/masterOrderModule.js
@@ -1,20 +1,26 @@
 angular.module('masterOrderForm', ['ngAnimate', 'ngSanitize','ui.bootstrap'])
     .controller('MasterOrderController', ['$http','$scope', function($http, $scope) {
+        $scope.buttonEnabled = false;
         $scope.getUsers = function(user) {
             return $http.get("/users/get/" + user).then(function (response) {
                 return response.data;
             }, function (exception) {
                 console.log(exception);
             })
+        };
+        $scope.typeHeadSelect=function (event) {
+            $scope.buttonEnabled = event;
         }
     }])
-    .controller("NotificationController", ['$http', '$scope', function ($http, $scope) {
+    .controller("NotificationController", ['$http', '$scope', '$window', function ($http, $scope, $window) {
         $scope.notifications = [];
         setInterval(function () {
             $http.get("/notifications/get").then(function (response) {
                 var array = response.data;
-                if (array.length !== 0) {
+                if (array && array.length !== 0) {
                     $scope.notifications = response.data;
+                } else {
+                    $window.location.reload();
                 }
             }, function (exception) {
                 console.log(exception);
